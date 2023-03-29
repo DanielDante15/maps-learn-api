b1b054511aef4c50dd6f78a6f9955fa522aea594
--global <--global>
2023-03-29 00:06:55 -0300

    fixing hosts from api

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kji=do62yv8r=#rl#=p1di5i^q0$g+be(7qdx3lkj@vpxcoq!0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.1.100']
 
 
 # Application definition
--- home/views.py
+++ home/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render
 from rest_framework.viewsets import *
-from rest_framework import response, permissions
-
+from rest_framework import response, permissions,status
+from rest_framework.decorators import api_view
 from rest_framework_nested import routers
 from .serializers import *
 
 
-# class ClienteAPIView(ModelViewSet):
-#     permission_classes = (permissions.IsAuthenticated, )
-#     def get(self, request):
-#         user = request.user
-#         serializer = ClienteSerializer(user)
-        
-#         return response.Response({'user': serializer.data})
-    
 class EnderecoAPIView(ModelViewSet):
     queryset = Endereco.objects.all()
     serializer_class = EnderecoSerializer
+
+    @api_view(['GET','PUT','DELETE'])
+    def endereco_detail(request,pk):
+        try:
+            endereco = Endereco.objects.get(pk=pk)
+        except Endereco.DoesNotExist:
+            return response.Response(status=status.HTTP_404_NOT_FOUND)
+        
+        if request.method == 'GET':
+            serializer = EnderecoSerializer(endereco)
+            return response.Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = EnderecoSerializer(endereco,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return response.Response(serializer.data)
+            return response.Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        elif request.method == 'DELETE':
+            endereco.delete()
+            return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
 
 class MotoristaAPIView(ModelViewSet):
     queryset = Motorista.objects.all()
