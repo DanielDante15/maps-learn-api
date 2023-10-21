902b81cbf91b82c1449770033499e89fa317c753
DanielDante15 <danieldantefm@gmail.com>
2023-10-20 22:10:41 -0300

    adding changes

--- api/settings.py
+++ api/settings.py
@@ -146,10 +146,6 @@ REST_FRAMEWORK = {
     ]
 }
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://127.0.0.1:9000",
-# ]
-
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_METHODS = [
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.contrib import admin
 from home.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from rest_framework_simplejwt.views import (
@@ -16,3 +18,6 @@ urlpatterns = [
     path('admin/',  admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
